2e1ab1c5303ccec5062246e1584945406a4f0d0b
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-19 16:19:40 -0700

    Add tag generation to index.js, change array to string in tests

--- generation/index.js
+++ generation/index.js
@@ -3,11 +3,23 @@ const path = require('path');
 
 const writeToFile = require('./writer.js');
 const createStock = require('./stock.js');
+const createTags = require('./tags.js');
 
 const stockStream = fs.createWriteStream(path.join(__dirname, '/data/stocks.json'));
 const tagStream = fs.createWriteStream(path.join(__dirname, '/data/tags.json'));
 
 const DOCUMENTS = Math.pow(10, 2);
 
-writeToFile(stockStream, createStock, DOCUMENTS);
-writeToFile(tagStream, '{"tag": "ayylmao"}', DOCUMENTS);
+writeToFile(stockStream, DOCUMENTS, {
+  data: createStock,
+  header: '[\n',
+  footer: ']\n',
+  comma: true
+});
+
+writeToFile(tagStream, createTags.collectionLength, {
+  data: ((idx) => createTags(idx, DOCUMENTS)),
+  header: '[\n',
+  footer: ']\n',
+  comma: true
+});
