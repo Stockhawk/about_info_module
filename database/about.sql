b5ad886e0a666ee3841d815b6fcf6943d0b27d8e
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-23 20:35:42 -0700

    Write about.sql to populate database with large dataset

--- database/about.sql
+++ database/about.sql
@@ -7,36 +7,66 @@ DROP TABLE IF EXISTS tags;
 
 CREATE TABLE IF NOT EXISTS stocks (
   symbol text,
-  av numeric(2),
+  av numeric,
   ceo text,
-  cost numeric(2),
+  cost numeric,
   description text,
   employees integer,
-  equity numeric(2),
+  equity numeric,
   founded integer,
-  high numeric(2),
+  high numeric,
   hqc text,
   hqs text,
-  low numeric(2),
-  mc numeric(2),
-  open numeric(2),
-  pd numeric(2),
-  per numeric(2),
+  low numeric,
+  mc numeric,
+  open numeric,
+  pd numeric,
+  per numeric,
   shares integer,
   tags text[],
-  tr numeric(2),
-  volume numeric(2),
-  yearhigh numeric(2),
-  yearlow numeric(2),
+  tr numeric,
+  volume numeric,
+  yearhigh numeric,
+  yearlow numeric,
   PRIMARY KEY (symbol)
 );
 
 CREATE TABLE IF NOT EXISTS tags (
   tag text,
   symbols text[5],
-  prices numeric(2)[],
+  prices numeric[],
   PRIMARY KEY (tag)
 );
 
-COPY tags FROM '/home/krise3/Project/about_info_module/generation/data/tags.csv'
+COPY tags FROM '/home/krise3/Project/about_info_module/generation/data/psql/tags.csv'
+  WITH (FORMAT csv, DELIMITER '|', HEADER true);
+
+COPY stocks FROM '/home/krise3/Project/about_info_module/generation/data/psql/stocks-0.csv'
+  WITH (FORMAT csv, DELIMITER '|', HEADER true);
+
+COPY stocks FROM '/home/krise3/Project/about_info_module/generation/data/psql/stocks-1.csv'
+  WITH (FORMAT csv, DELIMITER '|', HEADER true);
+
+COPY stocks FROM '/home/krise3/Project/about_info_module/generation/data/psql/stocks-2.csv'
+  WITH (FORMAT csv, DELIMITER '|', HEADER true);
+
+COPY stocks FROM '/home/krise3/Project/about_info_module/generation/data/psql/stocks-3.csv'
+  WITH (FORMAT csv, DELIMITER '|', HEADER true);
+
+COPY stocks FROM '/home/krise3/Project/about_info_module/generation/data/psql/stocks-4.csv'
+  WITH (FORMAT csv, DELIMITER '|', HEADER true);
+
+COPY stocks FROM '/home/krise3/Project/about_info_module/generation/data/psql/stocks-5.csv'
+  WITH (FORMAT csv, DELIMITER '|', HEADER true);
+
+COPY stocks FROM '/home/krise3/Project/about_info_module/generation/data/psql/stocks-6.csv'
+  WITH (FORMAT csv, DELIMITER '|', HEADER true);
+
+COPY stocks FROM '/home/krise3/Project/about_info_module/generation/data/psql/stocks-7.csv'
+  WITH (FORMAT csv, DELIMITER '|', HEADER true);
+
+COPY stocks FROM '/home/krise3/Project/about_info_module/generation/data/psql/stocks-8.csv'
+  WITH (FORMAT csv, DELIMITER '|', HEADER true);
+
+COPY stocks FROM '/home/krise3/Project/about_info_module/generation/data/psql/stocks-9.csv'
   WITH (FORMAT csv, DELIMITER '|', HEADER true);
