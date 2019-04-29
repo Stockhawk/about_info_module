8bbc8076c50ace84b0d10d2e9d5a772ad719b6d7
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-29 15:18:32 -0700

    Write GET controller for stocks route

--- server/controllers.js
+++ server/controllers.js
@@ -1,16 +1,29 @@
 const database = require('../database/connection.js');
 
-
-
 module.exports.quotes = {
-  get: (req, res) => {},
+  get: (req, res) => {
+    const symbol = req.params.symbol;
+    const keys = [ symbol[0], symbol[1], symbol[2], symbol.slice(3) ];
+    let query = '';
+
+    query += 'SELECT * FROM stocks WHERE ';
+    query += 'prefix1 = ? AND prefix2 = ? ';
+    query += 'AND prefix3 = ? AND suffix = ?';
+    
+    database.execute(query, keys, { prepare: true })
+    .then(results => res.send(results.rows))
+    .catch(error => res.status(500).end())
+  },
   post: (req, res) => {},
   put: (req, res) => {},
   delete: (req, res) => {},
 }
 
 module.exports.tags = {
-  get: (req, res) => {},
+  get: (req, res) => {
+    const symbol = req.params.symbol;
+    let query = ''
+  },
   post: (req, res) => {},
   put: (req, res) => {},
   delete: (req, res) => {},
