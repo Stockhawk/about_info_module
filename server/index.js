a2f3f660f3ab9343f7cdefe3b3b880916ee62cb6
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-29 14:49:02 -0700

    Refactor server app file to use router

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const router = require('./router.js');
 
 const compression = require('compression');
 
@@ -11,7 +12,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(compression());
 
-app.listen(port, () => console.log('APP IS LISTENING'));
+app.listen(port, () => console.log(`[Server] Listening on port ${port}`));
 
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 app.use('/stocks/:symbol', express.static(path.join(__dirname, '../client/dist')));
+
+app.use('/api', router);
