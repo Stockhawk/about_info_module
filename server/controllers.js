4dd5960b10660ae2b074d32dc00a9cc27429151d
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-04-29 14:47:56 -0700

    Add server controller stub functions

---
+++ server/controllers.js
@@ -0,0 +1,17 @@
+const database = require('../database/connection.js');
+
+
+
+module.exports.quotes = {
+  get: (req, res) => {},
+  post: (req, res) => {},
+  put: (req, res) => {},
+  delete: (req, res) => {},
+}
+
+module.exports.tags = {
+  get: (req, res) => {},
+  post: (req, res) => {},
+  put: (req, res) => {},
+  delete: (req, res) => {},
+}
